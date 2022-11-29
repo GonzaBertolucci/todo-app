2c28b44f6711fba1009ee38dd77df4fbd71d29ff
Gonzalo Bertolucci <gonzalobertolucci.gb@gmail.com>
2022-11-29 16:25:54 -0300

    clase04 completada

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//IIFE
+( () => {
 const btn = document.querySelector("[data-form-btn]");
 const input = document.querySelector("[data-form-input]");
 const createTask = (evento) => {
@@ -8,17 +10,33 @@ const createTask = (evento) => {
     const task = document.createElement("li");
     task.classList.add("card");
     input.value = "";
-    const content = `<div>
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${value}</span>
-  </div>
+    const taskContent = document.createElement("div");
+    taskContent.appendChild(checkComplete());
+    const titleTask = document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerText = value;
+    taskContent.appendChild(titleTask);
+    const content = `
   <i class="fas fa-trash-alt trashIcon icon"></i>`
-    task.innerHTML = content;
+    //task.innerHTML = content;
+    task.appendChild(taskContent);
     list.appendChild(task);
-    console.log(content);
 };
-
-console.log(btn);
 
 btn.addEventListener("click", createTask);
 
+const checkComplete = () => {
+    const i = document.createElement("i");
+    i.classList.add("far", "fa-check-square", "icon");
+    i.addEventListener("click", compleTask);
+    return i;
+}
+
+const compleTask = (event) => {
+  const element = event.target;
+  element.classList.toggle("fas");
+  element.classList.toggle("completeIcon");
+  element.classList.toggle("far");
+}
+
+}) ();
