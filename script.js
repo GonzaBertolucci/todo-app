0c6ba9c662e07afc8de5a6312595bf0766b8c39e
Gonzalo Bertolucci <gonzalobertolucci.gb@gmail.com>
2022-12-02 16:34:24 -0300

    Aula 4 COMPLETADA

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //IIFE
 import { addTask } from "./components/addTask.js";
-import { readTasks } from "./components/readTasks.js";
+import { displayTasks } from "./components/readTasks.js";
 
 const btn = document.querySelector("[data-form-btn]");
 const input = document.querySelector("[data-form-input]");
 
 btn.addEventListener("click", addTask);
 
-readTasks();
+displayTasks();
